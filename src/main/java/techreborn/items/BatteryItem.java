f6e8fec8f8eddefba67434dfa7f2db208a320741
Lorenz Voßgätter <lorenz.vossgaetter@web.de>
2021-12-20 18:04:40 +0100

    empty battery items can't be activated and notify the player

--- src/main/java/techreborn/items/BatteryItem.java
+++ src/main/java/techreborn/items/BatteryItem.java
@@ -33,7 +33,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
@@ -42,6 +44,7 @@ import org.jetbrains.annotations.Nullable;
 import reborncore.common.powerSystem.PowerSystem;
 import reborncore.common.powerSystem.RcEnergyItem;
 import reborncore.common.powerSystem.RcEnergyTier;
+import reborncore.common.util.ChatUtils;
 import reborncore.common.util.ItemUtils;
 import techreborn.TechReborn;
 import techreborn.utils.InitUtils;
@@ -65,8 +68,15 @@ public class BatteryItem extends Item implements RcEnergyItem {
 	public TypedActionResult<ItemStack> use(final World world, final PlayerEntity player, final Hand hand) {
 		final ItemStack stack = player.getStackInHand(hand);
 		if (player.isSneaking()) {
-			ItemUtils.switchActive(stack, 1, world.isClient, MessageIDs.poweredToolID);
-			return new TypedActionResult<>(ActionResult.SUCCESS, stack);
+			if(((RcEnergyItem) stack.getItem()).getStoredEnergy(stack) > 0) {
+				ItemUtils.switchActive(stack, 1, world.isClient, MessageIDs.poweredToolID);
+				return new TypedActionResult<>(ActionResult.SUCCESS, stack);
+			}else{
+				ChatUtils.sendNoSpamMessages(MessageIDs.poweredToolID,
+						new TranslatableText("techreborn.message.notEnoughEnergy").formatted(Formatting.RED));
+				return new TypedActionResult<>(ActionResult.PASS, stack);
+			}
+
 		}
 		return new TypedActionResult<>(ActionResult.PASS, stack);
 	}
